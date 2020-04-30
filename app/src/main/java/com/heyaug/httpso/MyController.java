78baf84b12991ceceff48ca8f154276c635334dd
heyang <hey@suosi.com.cn>
2020-04-30 18:51:25 +0800

    add HttpSo

---
+++ app/src/main/java/com/heyaug/httpso/MyController.java
@@ -0,0 +1,49 @@
+package com.heyaug.httpso;
+
+import com.douyin.gorgon;
+import com.yanzhenjie.andserver.annotation.*;
+import com.yanzhenjie.andserver.framework.body.JsonBody;
+import com.yanzhenjie.andserver.http.HttpResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+@Controller
+public class MyController {
+    private static HashMap<String, String> JsonObjectToHashMap(JSONObject jsonObj) throws JSONException {
+        HashMap<String, String> data = new HashMap<>();
+        Iterator<String> it = jsonObj.keys();
+        while (it.hasNext()) {
+            String key = it.next();
+            String value = jsonObj.get(key).toString();
+            data.put(key, value);
+        }
+        return data;
+    }
+
+    @GetMapping("/test")
+    void postSign(HttpResponse response) {
+        Map<String, String> signMap = new HashMap<>();
+        signMap.put("signature", "heyaug");
+        JSONObject jsonObject = new JSONObject(signMap);
+        response.setBody(new JsonBody(jsonObject));
+    }
+
+    @ResponseBody
+    @PostMapping("/gorgon")
+    String getNewGogon(@RequestBody String body) {
+        try {
+            JSONObject bd = new JSONObject(body);
+            String url = (String) bd.get("url");
+            JSONObject headers = (JSONObject) bd.get("headers");
+            return gorgon.getGorgon(url, JsonObjectToHashMap(headers));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/ss/sys/ces/a.java
@@ -0,0 +1,41 @@
+package com.ss.sys.ces;
+
+import android.content.Context;
+
+/**
+ * @author heyaug
+ */
+public final class a {
+    static {
+        try {
+            System.loadLibrary("cms");
+        } catch (UnsatisfiedLinkError ignored) {
+        }
+    }
+
+    public a() {
+        super();
+    }
+
+    private static void Bill() {
+    }
+
+    public final strictfp void Francies() {
+    }
+
+    public static void Louis() {
+    }
+
+    public static void Zeoy() {
+    }
+
+    public static native byte[] decode(int arg0, byte[] arg1);
+
+    public static native byte[] encode(byte[] arg0);
+
+    public static native byte[] leviathan(int arg0, int arg1, byte[] arg2);
+
+    public static native Object meta(int arg0, Context arg1, Object arg2);
+
+}
+
